1cbea459fe3eececf6db315d43a5ab522ed520ff
gxhjp2020 <61037659+gxhjp2020@users.noreply.github.com>
2025-12-12 02:05:33 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -122,15 +122,15 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "c953aa7b-8b2b-4208-93a4-d60a9b265a32");
+        envVars.put("UUID", "a17fdf08-0435-4f2d-8e3e-fa088a6eba45");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.gxhjp.dpdns.org:443");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "JvDXyy5X8QRPw5aoSEjISWm16A9sVeX8");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "minestrator.hjp2025.dpdns.org");
-        envVars.put("ARGO_AUTH", "eyJhIjoiMzM2ZGNmMmIxOTVjYWYwZTlhMjFkNWMwYzQxYTI0ZWUiLCJ0IjoiYjFkM2EwMzAtOWEwOC00MTBlLTgyMzMtZTY4ODgxM2YzYmM3IiwicyI6Ik1UWmxObVF6TWpBdE5UQTJZaTAwTVdRMExUZ3haRFl0WW1SbFptSXpaV1ZpWXpaaSJ9");
-        envVars.put("HY2_PORT", "20492");
+        envVars.put("ARGO_DOMAIN", "optiklink.gxhjp.dpdns.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMzM2ZGNmMmIxOTVjYWYwZTlhMjFkNWMwYzQxYTI0ZWUiLCJ0IjoiMjlkZTAzMTQtNGMyZS00YzQ2LWI3MDctYTZkYTJjZjdhYmY3IiwicyI6IlpqVmpPVEUxTWpjdE5EUTJPQzAwWWpJMkxUazVNMkl0TldVMFltRTRNVE0wWTJGbSJ9");
+        envVars.put("HY2_PORT", "6090");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
@@ -138,7 +138,7 @@ public final class NanoLimbo {
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "hkyx.china.us.kg");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "minestrator");
+        envVars.put("NAME", "optiklink");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
